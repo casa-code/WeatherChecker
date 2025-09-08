6165f728ce4d37e22948121707514d8ee75f3300
zombiewater <rafaelrodriguezcasanova@proton.me>
2025-09-07 20:08:46 -0400

    Cleaning Up Project, Adding Package Requirements.txt, Updateing .gitignore

--- files/DataClass.py
+++
@@ -1,4 +0,0 @@
-class Data:
-    def __init__(self, weather, msg):
-        self.data = weather
-        self.msg = msg
--- files/get_weather.py
+++ files/get_weather.py
@@ -4,7 +4,6 @@ def get_weather(lat, lon, api_key):
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
     response = requests.get(url)
     weather_json = response.json()
-    print(weather_json)
     return weather_json
 
 
